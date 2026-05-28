fb435bad82a2e1b8b0173ad2160a1dcf05ced560
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2026-05-28 03:56:05 +0000

    perbaikan responsiv

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,63 +2,108 @@
 
     <div class="container">
 
-        <!-- Mobile Toggle -->
+        <!-- ======================================
+             MOBILE TOGGLER
+        ======================================= -->
         <button
-            class="navbar-toggler"
+            class="navbar-toggler border-0 shadow-none"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#mainNavbar"
+            aria-controls="mainNavbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
         >
 
-            <i class="fa-solid fa-bars text-white"></i>
+            <i class="fa-solid fa-bars-staggered text-white"></i>
 
         </button>
 
-        <!-- Navbar -->
-        <div class="collapse navbar-collapse" id="mainNavbar">
+        <!-- ======================================
+             NAVBAR MENU
+        ======================================= -->
+        <div
+            class="collapse navbar-collapse"
+            id="mainNavbar"
+        >
 
             <ul class="navbar-nav mx-auto gap-lg-4">
 
                 <li class="nav-item">
-                    <a class="nav-link active" href="#">
+
+                    <a
+                        class="nav-link active"
+                        href="#"
+                    >
                         Beranda
                     </a>
+
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
+
+                    <a
+                        class="nav-link"
+                        href="#"
+                    >
                         Produk
                     </a>
+
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
+
+                    <a
+                        class="nav-link"
+                        href="#"
+                    >
                         Promo
                     </a>
+
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
+
+                    <a
+                        class="nav-link"
+                        href="#"
+                    >
                         Tentang Kami
                     </a>
+
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
+
+                    <a
+                        class="nav-link"
+                        href="#"
+                    >
                         Testimoni
                     </a>
+
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
+
+                    <a
+                        class="nav-link"
+                        href="#"
+                    >
                         Blog
                     </a>
+
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
+
+                    <a
+                        class="nav-link"
+                        href="#"
+                    >
                         Kontak
                     </a>
+
                 </li>
 
             </ul>
