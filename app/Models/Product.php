8a593df189dd8352468d44f53e8a01f22003e1fc
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2026-05-28 15:31:03 +0000

    edit catecgory and product models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'category_id',
+        'title',
+        'slug',
+        'description',
+        'price',
+        'stock',
+        'image',
+        'is_featured',
+        'is_active',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
