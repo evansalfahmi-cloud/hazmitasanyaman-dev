e6b290b1e938d24cf67d6f52f43a310ff6d4eaac
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2026-05-28 02:55:04 +0000

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,189 @@
-<div class="footer">
-    Footer
-</div>
+<footer class="footer-wrapper">
+
+    <div class="container">
+
+        <div class="row gy-5">
+
+            <!-- ======================================
+                 BRAND
+            ======================================= -->
+            <div class="col-lg-4">
+
+                <div class="footer-brand">
+
+                    <h3>
+                        Hazmi Tas Anyaman
+                    </h3>
+
+                    <p>
+
+                        Produk tas anyaman handmade berkualitas
+                        dengan desain modern, elegan,
+                        dan bernuansa artisan.
+
+                    </p>
+
+                    <div class="footer-social d-flex align-items-center gap-3">
+
+                        <a href="#">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+
+                        <a href="#">
+                            <i class="fa-brands fa-facebook"></i>
+                        </a>
+
+                        <a href="#">
+                            <i class="fa-brands fa-tiktok"></i>
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- ======================================
+                 MENU
+            ======================================= -->
+            <div class="col-lg-2">
+
+                <div class="footer-menu">
+
+                    <h5>
+                        Menu
+                    </h5>
+
+                    <ul>
+
+                        <li>
+                            <a href="#">
+                                Beranda
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                Produk
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                Promo
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                Testimoni
+                            </a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+            <!-- ======================================
+                 BANTUAN
+            ======================================= -->
+            <div class="col-lg-3">
+
+                <div class="footer-menu">
+
+                    <h5>
+                        Bantuan
+                    </h5>
+
+                    <ul>
+
+                        <li>
+                            <a href="#">
+                                FAQ
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                Cara Pemesanan
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                Kebijakan Privasi
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                Hubungi Kami
+                            </a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+            <!-- ======================================
+                 KONTAK
+            ======================================= -->
+            <div class="col-lg-3">
+
+                <div class="footer-contact">
+
+                    <h5>
+                        Kontak
+                    </h5>
+
+                    <p>
+
+                        <i class="fa-solid fa-phone me-2"></i>
+
+                        (021) 1234 5678
+
+                    </p>
+
+                    <p>
+
+                        <i class="fa-solid fa-envelope me-2"></i>
+
+                        info@hazmitasanyaman.id
+
+                    </p>
+
+                    <p>
+
+                        <i class="fa-solid fa-location-dot me-2"></i>
+
+                        Lampung, Indonesia
+
+                    </p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <!-- ======================================
+             COPYRIGHT
+        ======================================= -->
+        <div class="footer-bottom">
+
+            <p>
+
+                © {{ date('Y') }} Hazmi Tas Anyaman.
+                All rights reserved.
+
+            </p>
+
+        </div>
+
+    </div>
+
+</footer>
