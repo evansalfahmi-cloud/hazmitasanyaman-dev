c599d2c047ad76399637de280b8526400e564190
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2026-05-28 11:08:53 +0000

    tambah tabel kategori dan produk

---
+++ database/migrations/2026_05_28_110358_create_products_table.php
@@ -0,0 +1,86 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        
+        Schema::create('products', function (Blueprint $table) {
+
+            $table->id();
+
+            /*
+            |--------------------------------------------------------------------------
+            | RELATION
+            |--------------------------------------------------------------------------
+            */
+
+            $table->foreignId('category_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+
+            /*
+            |--------------------------------------------------------------------------
+            | PRODUCT
+            |--------------------------------------------------------------------------
+            */
+
+            $table->string('title');
+
+            $table->string('slug')->unique();
+
+            $table->text('description')->nullable();
+
+            $table->integer('price')->default(0);
+
+            $table->integer('stock')->default(0);
+
+            $table->string('image')->nullable();
+
+
+            /*
+            |--------------------------------------------------------------------------
+            | FEATURED PRODUCT
+            |--------------------------------------------------------------------------
+            */
+
+            $table->boolean('is_featured')->default(false);
+
+
+            /*
+            |--------------------------------------------------------------------------
+            | STATUS
+            |--------------------------------------------------------------------------
+            */
+
+            $table->boolean('is_active')->default(true);
+
+
+            /*
+            |--------------------------------------------------------------------------
+            | TIMESTAMP
+            |--------------------------------------------------------------------------
+            */
+
+            $table->timestamps();
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
