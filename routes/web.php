640d0e6f784a19b54192299153c84f19dbccef91
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2026-05-30 02:38:51 +0000

    add routes category web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\CategoryController;
 
 Route::get('/', [HomeController::class, 'index']);
 
@@ -14,6 +15,12 @@ Route::middleware(['auth'])
 
         Route::get('/dashboard', [DashboardController::class, 'index'])
             ->name('admin.dashboard');
+
+        /*
+        |--------------------------------------------------------------------------
+        | Produk
+        |--------------------------------------------------------------------------
+        */
 
         Route::get('/products', [ProductController::class, 'index'])
             ->name('admin.products.index');
@@ -27,12 +34,35 @@ Route::middleware(['auth'])
         Route::get('/products/{product}/edit', [ProductController::class, 'edit'])
             ->name('admin.products.edit');
 
-        Route::put('/products/{product}',[ProductController::class, 'update'])
+        Route::put('/products/{product}', [ProductController::class, 'update'])
             ->name('admin.products.update');
-            
+
         Route::delete('/products/{product}', [ProductController::class, 'destroy'])
             ->name('admin.products.destroy');
 
+        /*
+        |--------------------------------------------------------------------------
+        | Kategori
+        |--------------------------------------------------------------------------
+        */
+
+        Route::get('/categories', [CategoryController::class, 'index'])
+            ->name('admin.categories.index');
+
+        Route::get('/categories/create', [CategoryController::class, 'create'])
+            ->name('admin.categories.create');
+
+        Route::post('/categories', [CategoryController::class, 'store'])
+            ->name('admin.categories.store');
+
+        Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])
+            ->name('admin.categories.edit');
+
+        Route::put('/categories/{category}', [CategoryController::class, 'update'])
+            ->name('admin.categories.update');
+
+        Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])
+            ->name('admin.categories.destroy');
     });
 
 require __DIR__.'/auth.php';
