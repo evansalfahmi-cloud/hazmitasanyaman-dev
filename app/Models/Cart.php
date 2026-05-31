0dbbe3799d524064ea45268668c402e361e4c175
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2026-05-31 05:23:43 +0000

    add Models/cart dan cartitem

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Cart extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relasi ke User
+    |--------------------------------------------------------------------------
+    */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relasi ke Cart Item
+    |--------------------------------------------------------------------------
+    */
+    public function items()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CartItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'cart_id',
+        'product_id',
+        'quantity',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relasi ke Cart
+    |--------------------------------------------------------------------------
+    */
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relasi ke Product
+    |--------------------------------------------------------------------------
+    */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
